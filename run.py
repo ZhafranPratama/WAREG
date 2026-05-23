58ed6a30bd4e9181add12d542c31bf54b1bc4881
Ray Anargya <anargya20@gmail.com>
2026-05-23 12:51:06 +0700

    fixing merge issue

--- run.py
+++ run.py
@@ -4,12 +4,8 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 7860))
     app.run(host='0.0.0.0', port=port)
 
-<<<<<<< HEAD
-#Raissa Anargya Putra
-=======
 #salwa
 #Nada nyobain
 #salwa
 #muti coba coba 
 #Raissa Anargya Putra
->>>>>>> cdb221da6e1f47800e5e431c6101c60afa3438ee
